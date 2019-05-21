8c71916a56927756da4ffa123ab88cba2bef1185
aeisha simmons <ashicks2017@outlook.com>
2019-05-21 15:55:52 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -89,10 +89,11 @@ def adding_matz
           :languages => ["C"]
         }
      }        
-     
-    
+ programmer_hash[:yukihiro_matsumoto] = {:known_for => "Rudy", :languages => ["LISP", "C"] }   
+  programmer_hash  
 end
 
+ 
 
           
 def changing_alan
