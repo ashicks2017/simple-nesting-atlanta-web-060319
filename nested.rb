fec6d3fceeda23014d63c113fb8ec8316c439e1d
aeisha simmons <ashicks2017@outlook.com>
2019-05-21 15:48:41 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -89,11 +89,7 @@ def adding_matz
           :languages => ["C"]
         }
      }        
-     :yukihiro_matsumoto => {
-    :known_for => "Ruby",
-    :languages => ["LISP", "C"]
- }
-
+     
     
 end
 
